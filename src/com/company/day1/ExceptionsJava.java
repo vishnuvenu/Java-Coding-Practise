1d546d624c7d0ab26727bdfa5a8d75d8e46653b1
vishnuvenu <vishnuvenugopal73@gmail.com>
2023-12-16 21:11:56 +0530

    exceptions in java

---
+++ src/com/company/day1/ExceptionsJava.java
@@ -0,0 +1,22 @@
+package com.company.day1;
+
+public class ExceptionsJava {
+
+    public static void main(String[] args) {
+
+        try {
+
+            System.out.println("test");
+        }
+        //catch (Exception e){}
+        finally {
+
+        }
+
+        /**
+         *
+         * try should have used either with catch or finally or both
+         * try block can't exist without
+         */
+    }
+}
